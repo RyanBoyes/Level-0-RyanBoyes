0dbfb8fd02eefff74083e5e32557014720f5cb06
Anthony Cerruti <aprime2003@gmail.com>
2017-04-09 11:31:54 -0700

    Eclipse

---
+++ Level-0/src/Ninety9Bottles.java
@@ -0,0 +1,50 @@
+
+public class Ninety9Bottles {
+	public static void main(String[] args) {
+		for (int i = 99; i > 0; i--) {
+			
+			if(i>1){
+			
+			
+			System.out.println(i+" bottles of beer on the wall. "+i+
+					" bottles of beer. Take one down, pass it around, "+(i-1)+
+					" bottles of beer on the wall.");
+			
+		}
+			
+			
+			
+						
+			
+			
+			
+			else{
+				
+			System.out.println("1 bottle of beer on the wall. 1 bottle of beer. "
+					+ "Take one down, pass it around, no more bottles of beer on the wall.");
+		}
+		
+		
+		
+		
+		}
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+
+
+
+
+
+
+
+
+}
