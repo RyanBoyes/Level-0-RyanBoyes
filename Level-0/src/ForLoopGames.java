c41fd6e6c979bd2c8d63c6e3a7658ad75c87e53d
CactusCat <wjmorris2004@gmail.com>
2017-07-16 11:30:59 -0700

    ForLoopThangs
    ya

---
+++ Level-0/src/ForLoopGames.java
@@ -0,0 +1,156 @@
+import javax.swing.JOptionPane;
+
+public class ForLoopGames {
+	public static void main(String[] args) {
+		
+	
+	
+	for(int i=0;i<101;i++){
+		System.out.println(i);
+		}
+	
+	for(int o=100;o>-1;o--){
+		System.out.println(o);
+
+	}
+	
+	for(int p=2;p<=100;p+=2){
+	System.out.println(p);	
+	
+	}
+	
+	for(int q=1;q<=99;q+=2){
+	System.out.println(q);
+	}
+	
+	for(int w=0;w<=500;w++){
+	if(w%2==1){
+	System.out.println(w+" odd");
+	}
+	else{
+	System.out.println(w+" even");	
+	}
+	
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	for(int e=7;e<=777;e+=7){
+		System.out.println(e);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	for(int r=2004;r<=2017;r++){
+		System.out.println("in "+ r+", I was "+ (r-2004) +" years old.");
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	for(int t=0;t<=2;t++){
+		for(int to=0;to<=2;to++){
+			System.out.println(t+" "+to);
+		}
+	}
+		
+		
+	for(int y=1;y<=7;y+=3){
+		for(int yy=2;yy<=8;yy+=3){
+			for(int yyy=3;yyy<=9;yyy+=3){
+				
+				System.out.println(y+" "+yy+" "+yyy);
+		}
+		
+		
+		
+		}
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+}
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
